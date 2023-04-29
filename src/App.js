9bb5efade29da1b94cdddc15cfd1070fdccf0aa5
Aman <razaaman57@gmail.com>
2023-04-29 20:15:01 +0530

    use usestate to set value of weight and height

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const[weight ,setweight] = useState(0);
+  const[height ,setheight] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h2>BMI Calculator</h2>
+        <form action="">
+          <div>
+            <label htmlFor="">Weight (lbs)</label>
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Enter your Weight"
+            value={weight}
+            />
+          </div>
+          <div>
+            <label htmlFor="">Height</label>
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Enter your height"
+              value={height}
+            />
+          </div>
+          <div>
+            <button className="btn btn-outline-success my-2">Submit</button>
+          </div>
+          <div>
+            <button className="btn btn-outline-success my-2">Reload</button>
+          </div>
+          <div className="center">
+            <h3>Your BMI is:</h3>
+          </div>
+          <div className="center">
+            <h3>Your are:</h3>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
